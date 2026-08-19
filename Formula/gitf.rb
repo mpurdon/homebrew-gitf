6d561120b8af9d807eff9e33bf462841e09b13bd
Ghost-Factory <factory@ghostinthefactory.com>
2026-08-19 05:54:24 +0000

    gitf 0.65.127

--- Formula/gitf.rb
+++ Formula/gitf.rb
@@ -1,9 +1,9 @@
 class Gitf < Formula
   desc "Ghost in the Factory - autonomous AI software factory CLI"
   homepage "https://ghostinthefactory.com"
-  url "https://github.com/mpurdon/gitf/releases/download/v0.65.126/gitf"
-  sha256 "ca30de0344fee283618ed736cf27f54a46632862e491566a054aa37975d254a0"
-  version "0.65.126"
+  url "https://github.com/mpurdon/gitf/releases/download/v0.65.127/gitf"
+  sha256 "1d17398732cebcd69bf59ec8cc774c6096a348fa0b7116f3769927821d47f401"
+  version "0.65.127"
 
   depends_on "erlang"
 
